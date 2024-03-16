6cc022825ef0ec3daddb58b51646f5c900f80013
Huaqing Jin <hjin@dev1.wynton.ucsf.edu>
2024-03-16 00:23:11 -0700

    ...

--- bash_scripts/wynton/sub_batch_lei.sh
+++ bash_scripts/wynton/sub_batch_lei.sh
@@ -1,7 +1,7 @@
 for nT in 400 200 100; do
     for lr in 1e-1 1e-2 ; do
         for d in 10 100; do
-            setting=4
+            setting=1
             job_name=leiS${setting}_lr${lr}_nT${nT}_d${d}
             qsub -N $job_name simu_lei.sh $nT $lr $setting $d
         done
